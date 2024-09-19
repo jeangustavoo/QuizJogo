608d62f5af98eabd09310e73ef9b9e358018c46b
quizjogocomeço <jeangustavofut@gmail.com>
2024-09-19 09:05:21 -0300

    começando

---
+++ QuizJogo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizJogo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ QuizJogo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace QuizJogo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
